4baf8149755c936b9a0aaad31ab75da383a99c59
gokcankrc <gokcankrc@gmail.com>
2025-01-18 22:09:44 +0300

    Temporarily made containment wall breaks trigger breakout

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,5 +1,7 @@
 using System;
 using Ky;
+using UnityEngine;
+using Logger = Ky.Logger;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -8,13 +10,16 @@ public class GameManager : Singleton<GameManager>
 
     public void ContainmentWallBroken()
     {
-        ChangeState(GameState.BrokeContainmentWalls);
+        // ChangeState(GameState.BrokeContainmentWalls);
+        ChangeState(GameState.InBreakout);
         onContainmentWallBroken?.Invoke();
     }
 
     private void ChangeState(GameState state)
     {
-        Logger.Log($"Game state: <color=white>{gameState} -> {state}.", Logger.DomainType.System);
+        Logger.Log($"Game state: " +
+                   $"{Color.white.EncapsulateString(gameState.ToString())} -> " +
+                   $"{Color.white.EncapsulateString(state.ToString())}.", Logger.DomainType.System);
         gameState = state;
     }
 }
