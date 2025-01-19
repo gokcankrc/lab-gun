9c740f67675857c639741c2ee6fb3a75ecc5388d
gokcankrc <gokcankrc@gmail.com>
2025-01-19 17:54:04 +0300

    Breakable walls expanded

--- Assets/Scripts/Breakables/ContainmentWall.cs
+++ Assets/Scripts/Breakables/ContainmentWall.cs
@@ -1,46 +1,8 @@
-using UnityEngine;
-using Logger = Ky.Logger;
-
-public class ContainmentWall : Breakable
+public class ContainmentWall : SpeedBreakable
 {
-    [SerializeField] private GameObject destroyedDecalPrefab;
-    [SerializeField] private int health = 5;
-    [SerializeField] private float hurtingSpeedThreshold;
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        var player = other.transform.GetComponent<Player>();
-        if (player != null)
-        {
-            var speed = other.relativeVelocity.magnitude;
-            if (speed > hurtingSpeedThreshold)
-            {
-                Debug.Log($"<color=green>Passes, {speed}</color>");
-                // TODO: Sound
-                TakeDamage();
-            }
-            else
-            {
-                Debug.Log($"<color=red>Doesn't pass, {speed}</color>");
-                // TODO: Sound
-            }
-        }
-    }
-
-    private void TakeDamage()
-    {
-        // TODO: Show cracks
-        health -= 1;
-        if (health <= 0)
-        {
-            Break();
-        }
-    }
-
-    private void Break()
+    protected override void Break()
     {
         GameManager.I.ContainmentWallBroken();
-        Instantiate(destroyedDecalPrefab, transform.position, Quaternion.identity, DecalParent.I);
-        Destroy(gameObject);
+        base.Break();
     }
 }
