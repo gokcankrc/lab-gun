7a6e91e110f3d598c84fb35d93f988fcfbf23035
mecha <jonkusar4@gmail.com>
2025-01-31 00:56:21 +0100

    Hints and texts added
    Hint storage script attached to Hint container game object in main scene

---
+++ Assets/Scripts/HintsStorage.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using UnityEngine;
+
+public class HintStorage : MonoBehaviour
+{
+    public string[] hints = {
+        "Hint 1 - lvl 1 (note): Master your movements! Use W, A, S, D to walk, and hold Left Click to roll around.",
+        "Hint 2 - lvl 1 (friendly scientist): Watch out for the enemies, if they notice you they will try to capture you",
+        "Hint 3 - lvl 1 (note or FS): Pay attention to the colors of objects they might prove cruical for escaping",
+        "Hint 4 - lvl 2 (note or FS): Some doors might open under the right conditions.",
+        "Hint 5 - lvl x (xxx): Try rolling at a high speed and hitting a red door",
+        "Hint 6 - lvl (xxx): Colored objects can assign you a tag to unlock certain door BUT pay attention as hitting other objects or projectiles as they can strip you of your tag",
+        "Hint 7 - lvl (xxx): Some doors need multiples of the same tag to be unlocked",
+        "Hint 8 - lvl (xxx): Every choice you make shapes your fate. Explore different paths to uncover multiple endings!",
+        "Hint 9 - lvl (xxx): Not all enemies are the same! Watch out and stay vigilant",
+        "Hint 10 - lvl (xxx): Pay attention to your speed as it can help you deal more damage ",
+        "Hint 11 - lvl (xxx): Use your time rewind ability to return to your spawn and try a different route",
+        "Hint 12 - lvl (xxx): Getting hit by a projectile can strip you of your tag",
+        "Hint 13 - lvl 5 (note): Sacrificing your allies may lead to a quicker escape.",
+        "ok these were the hints, feel free to change them up or add extras as you go since you probably know better than me where something would be useful",
+
+        "JOKES and CHATTER",
+        "You know what the world needs? Goats with silk in their milk! ",
+        "There’s no way these walls could be breached; they're the peak of human engineering... until someone invents a spider goat with a hammer.",
+        "These walls could never be breached, they are the peak of human engineerign",
+        "Check boxes, check boxes, they're everywhere! Can't escape them, can't ignore them, they're watching me... did I check that one? Oh no, I missed it—it's coming for me!",
+        "Delivered the coffee to the boss—not the one Frank made, though. That stuff's not coffee. it's a cry for help.",
+        "I'd make a pun about clones...but you'd just copy it.",
+        "If I was cloned, I’d be beside myself!... Oh right I am!",
+        "You're so good looking, I'm starting to think we were cut from the same cloth!",
+        "There is a party on friday, just dont tell Fred, he somehow makes everything a duplicate disaster.",
+        "If the mutant with the extra eyes starts watching me, I will lose my shit.",
+        "Conspiracy theories are a lot like moon landings. They're all fake",
+        "Why do programmers prefer dark mode? Because the light attracts bugs!",
+        "They told me this was a ‘top-secret’ mutant experiment. I thought I was gonna see something cool. Instead, we got a sentient blob with commitment issues.",
+        "Did you hear about the horrible accident where the physicist accidentally chilled himself to absolute zero? He's 0K now.",
+        "Don't trust atoms, they make up everything.",
+        "How is project Rainbow going, are the tadpoles showing any progress?",
+        "We’ve got to ease up on the cameras; the pigeons are starting to look way too suspicious.",
+
+        "Ok Thats what i managed to scramble together i hope any of it is usefull"
+
+    };
+}
+
