9bc62641d97741b1a4c920f229c50e0abd401640
gokcankrc <gokcankrc@gmail.com>
2025-01-31 14:09:27 +0300

    TextPopup debug shows

--- Assets/Scripts/Entities/TextPopup.cs
+++ Assets/Scripts/Entities/TextPopup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class TextPopup : MonoBehaviour
@@ -8,10 +9,26 @@ public class TextPopup : MonoBehaviour
     public TextPopupHandler prefab;
     public TextMode mode;
     public TextCondition condition;
+    [SerializeField, HideInInspector] private TextPopupHandler debug;
 
     private void Start()
     {
         TextPopupManager.I.SetUp(this);
+    }
+
+    [Button]
+    private void ShowDebug()
+    {
+        debug = Instantiate(prefab, transform.position + Vector3.back, Quaternion.identity, transform);
+        debug.gameObject.AddComponent<DestroyOnStart>();
+    }
+
+    private void HideDebug()
+    {
+        if (debug)
+        {
+            Destroy(debug.gameObject);
+        }
     }
 
     [Serializable]
